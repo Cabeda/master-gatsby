a1234e99db9573e4922139e6271d1d599ec0149f
Cabeda <jecabeda@gmail.com>
2020-10-24 09:51:24 +0100

    Lesson 47 done

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -6,7 +6,7 @@ import Logo from './Logo';
 const NavStyles = styled.nav`
   margin-bottom: 3 rem;
   .logo {
-    transform: translateY(-25%);
+    /* transform: translateY(-25%); */
   }
   ul {
     margin: 0;
